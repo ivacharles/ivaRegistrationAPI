71ca6dfc4fafba32c47d5b626e7c2479d7a487c2
Iva Lesperance Charles <Iva.LesperanceCharles@mail.citytech.cuny.edu>
2021-07-02 22:47:21 -0400

    Initial commit

---
+++ src/main/java/iva/lesperance/api/tutorial/IvaRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package iva.lesperance.api.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvaRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvaRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iva/lesperance/api/tutorial/IvaRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package iva.lesperance.api.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvaRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
